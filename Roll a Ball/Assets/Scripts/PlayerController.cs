dcba8b7db4c98b0ea65aa6fb14751ddef56b9a1e
Felipe <feliperpv@hotmail.com>
2021-03-13 17:52:37 -0300

    dethmenu - not working

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -42,10 +42,9 @@ public class PlayerController : MonoBehaviour
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 12)
-        {
-            winText.text = "You Win!";
-        }
+        countText.text = count.ToString();
+       
+        winText.text = count.ToString();
+        
     }
 }
