0e82332bcf2b6415429806eb3b1524377fa5c895
Felipe <feliperpv@hotmail.com>
2021-03-15 17:23:55 -0300

    Add Main Menu
    -RestartButtonScript renamed to DeathMenu
    -StarMenu added
    -Main Menu built
    
    Bugs:
    -lights go crazy when loading a scene

---
+++ Roll a Ball/Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+
+    public void restartScene()
+    {
+        SceneManager.LoadScene("MiniGame");
+    }
+
+    public void backToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
