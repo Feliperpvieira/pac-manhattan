a7ce77d5f70e0fa8f149d0e8b221a166b502930f
Felipe <feliperpv@hotmail.com>
2021-03-15 20:42:12 -0300

    spawn objects press Q

---
+++ Roll a Ball/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+
+    public GameObject ItemPrefab;
+
+    public Vector3 center;
+    public Vector3 size;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnItem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            SpawnItem();
+        
+    }
+
+    public void SpawnItem()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+        Instantiate(ItemPrefab, pos, Quaternion.identity);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+
+    }
+}
