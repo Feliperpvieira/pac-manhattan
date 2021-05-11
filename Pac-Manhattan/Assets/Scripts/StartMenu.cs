a03b27e5b24b01cf2721eede5008d329ecc2b30a
Felipe <47862856+Feliperpvieira@users.noreply.github.com>
2021-05-11 15:14:01 -0300

    add settings menu

--- Pac-Manhattan/Assets/Scripts/StartMenu.cs
+++ Pac-Manhattan/Assets/Scripts/StartMenu.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+
+    public GameObject SettingsMenu, PlayButton, SettingsButton;
+    public Text highscoreText;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("MiniGame");
@@ -13,5 +18,26 @@ public class StartMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        SettingsMenu.SetActive(true);
+        PlayButton.SetActive(false);
+        SettingsButton.SetActive(false);
+        highscoreText.text = "High Score: " + PlayerPrefs.GetInt("Highscore").ToString();
+    }
+
+    public void CloseSettings()
+    {
+        SettingsMenu.SetActive(false);
+        PlayButton.SetActive(true);
+        SettingsButton.SetActive(true);
+    }
+
+    public void ResetSave()
+    {
+        PlayerPrefs.DeleteAll();
+        highscoreText.text = "High Score: " + PlayerPrefs.GetInt("Highscore", 0).ToString();
     }
 }
